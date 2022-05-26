658ca52b6e5b8d9860949ab09c8aa2ace73f21e7
Pedro Victor <pedro.mattis.df@gmail.com>
2022-05-25 21:30:48 -0300

    Base do projeto corrigida - Espinha dorsal inicial feita com susucesso

---
+++ src/main/java/com/tjma/toadalab/RoboTriadorApiApplication.java
@@ -0,0 +1,13 @@
+package main.java.com.tjma.toadalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoboTriadorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoboTriadorApiApplication.class, args);
+	}
+
+}
