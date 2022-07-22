217c2128945a779d3937fb32aa56c80e019fb467
Pedrão Wayne <pedro.mattis.df@gmail.com>
2022-07-21 22:42:14 -0300

    correção na tabela de robôs e correção na tela de tarefas: inserindo um scrollpane para as tarefas exibidas

--- src/main/java/com/tjma/toadalab/Repositories/ExecucaoRepository.java
+++ src/main/java/com/tjma/toadalab/Repositories/ExecucaoRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import main.java.com.tjma.toadalab.Models.Execucao;
-
+//Repositório de Execução
 @Repository
 public interface ExecucaoRepository extends JpaRepository<Execucao, Long>{
 	
