9fc8b83e9fc42c970c0cc77f1cc0ce398ac1e158
Pedro Dantas <pedro.mattis.df@gmail.com>
2023-10-06 20:53:10 -0300

    Correção para merge com o remoto

--- src/main/java/main/java/com/tjma/toadalab/dtos/DistritoRecordDTO.java
+++ src/main/java/main/java/com/tjma/toadalab/dtos/DistritoRecordDTO.java
@@ -1,9 +1,5 @@
 package main.java.com.tjma.toadalab.dtos;
 
-import java.util.List;
-
-import main.java.com.tjma.toadalab.models.Mandado;
-
 public record DistritoRecordDTO(String nomeDistrito, String codigo, String bairro) {
 
 }
--- src/main/java/main/java/com/tjma/toadalab/models/Distrito.java
+++ src/main/java/main/java/com/tjma/toadalab/models/Distrito.java
@@ -1,13 +1,10 @@
 package main.java.com.tjma.toadalab.models;
 
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
