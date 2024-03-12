50082c5a482f22f40edc04972de04492b52538bf
Pedro Dantas <pedro.mattis.df@gmail.com>
2024-03-12 20:08:39 -0300

    Merge para o remoto

--- src/main/java/main/java/com/tjma/toadalab/dtos/DistritoRecordDTO.java
+++ src/main/java/main/java/com/tjma/toadalab/dtos/DistritoRecordDTO.java
@@ -1,9 +1,6 @@
 package main.java.com.tjma.toadalab.dtos;
 
-<<<<<<< HEAD
-public record DistritoRecordDTO(String nomeDistrito, String codigo, String bairro) {
-=======
+
 public record DistritoRecordDTO(String nomeDistrito, String codigo) {
->>>>>>> cc7626981823cff0e60774922cab781f1bce0e25
 
 }
